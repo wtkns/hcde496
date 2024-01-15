b39068f9c692007bd794fb588ee9d35b45ad5c83
james watkins <jms@wtkns.com>
2024-01-15 11:34:43 -0800

    moving for project architecture

--- sd_api/api-call.py
+++ sd_api/api-call.py
@@ -2,12 +2,14 @@ import json
 import requests
 import io
 import base64
+import time
 from PIL import Image
 
 url = "http://127.0.0.1:7860"
+timestr = time.strftime("%Y%m%d-%H%M%S")
 
 payload = {
-    "prompt": "puppy dog",
+    "prompt": "puppy dog playing with random object",
     "steps": 5
 }
 
@@ -16,4 +18,4 @@ response = requests.post(url=f'{url}/sdapi/v1/txt2img', json=payload)
 r = response.json()
 
 image = Image.open(io.BytesIO(base64.b64decode(r['images'][0])))
-image.save('output.png')
+image.save('output' + timestr + '.png')
